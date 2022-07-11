0f26222699f401a4db12348a39ceeb6e99758730
起风了 zero <1124078695@qq.com>
2022-07-11 22:12:44 +0800

    second commit

--- src/main/java/com/example/work/GitTest.java
+++ src/main/java/com/example/work/GitTest.java
@@ -5,7 +5,7 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("HelloWord");
 
-        System.out.println("springBoot1");
+        System.out.println("springBoot");
     }
 
 }
