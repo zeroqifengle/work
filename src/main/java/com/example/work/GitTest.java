52aee3bb688407dea16f6ee018e2fdd27bb32baa
起风了 zero <1124078695@qq.com>
2022-07-11 22:12:05 +0800

    first commit

---
+++ src/main/java/com/example/work/GitTest.java
@@ -0,0 +1,11 @@
+package com.example.work;
+
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("HelloWord");
+
+        System.out.println("springBoot1");
+    }
+
+}
